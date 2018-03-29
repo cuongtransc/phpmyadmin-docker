53b388e33099d5ebd7f38d37f21acbe1ee6e25e8
Cuong Tran <cuongtransc@gmail.com>
2018-03-29 15:16:28 +0700

    Update docker-entrypoint.sh: use command php5 instead of php

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,7 @@ if [ "$1" = 'phpmyadmin' ]; then
 EOT
     fi
 
-    exec php -S 0.0.0.0:80 -t /www/ \
+    exec php5 -S 0.0.0.0:80 -t /www/ \
         -d upload_max_filesize=$PHP_UPLOAD_MAX_FILESIZE \
         -d post_max_size=$PHP_UPLOAD_MAX_FILESIZE \
         -d max_input_vars=$PHP_MAX_INPUT_VARS \
